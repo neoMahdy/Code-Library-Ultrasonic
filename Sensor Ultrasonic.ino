893455f2377270de8092702c1dc8be6eed748fff
Taufik Nur Mahdy <50261051+neoMahdy@users.noreply.github.com>
2020-02-02 16:24:29 +0700

    Add files via upload

---
+++ Sensor Ultrasonic.ino
@@ -0,0 +1,32 @@
+/*
+      neo Mahdy 
+  Penggaris Digital
+  Sensor Ultrasonic
+                         */
+
+#define trig 12
+#define echo 13
+long jarak, durasi;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trig, LOW);
+  durasi = pulseIn(echo, HIGH);
+  jarak = durasi * 0.034 / 2;
+  
+  Serial.print("Jarak : ");
+  Serial.print(jarak);
+  Serial.println(" Cm");
+  delay(500);
+}
